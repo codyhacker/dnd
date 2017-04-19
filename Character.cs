2c73dbdf16138689c864a987cf9a250ab2e45ef7
nick <ncyates@users.noreply.github.com>
2017-04-18 19:36:13 -0700

    initial commit of essential files

---
+++ Character.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dungeonsDragons
+{
+    public class Character : Unit
+    {
+
+        //additional fields
+        private int lvl;
+        private int exp;
+        private int score;
+        public Character()
+        {
+            lvl = 0;
+            exp = 0;
+            score = 0;
+            isGood = true;
+        }
+
+        public int getLvl()
+        {
+            return lvl;
+        }
+        public int getExp()
+        {
+            return exp;
+        }
+        public int getScore()
+        {
+            return score;
+        }
+
+        public void setlvl(int l)
+        {
+            lvl = l;
+        }
+        public void setExp(int e)
+        {
+            exp = e;
+        }
+        public void setScore(int s)
+        {
+            score = s;
+        }
+    }
+}
---
+++ Unit.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dungeonsDragons
+{
+    public class Unit
+    {
+        //fields for the unit class
+        private string name;
+        private int hp;
+        private int strength;
+        private int speed;
+        private int defense;
+        private int dexterity;
+        private List<Object> inventory;
+        protected bool isGood;
+
+        //get methods
+        public string getName()
+        {
+            return name;
+        }
+        public int getHP()
+        {
+            return hp;
+        }
+        public int getStr()
+        {
+            return strength;
+        }
+        public int getSpd()
+        {
+            return speed;
+        }
+        public int getDef()
+        {
+            return defense;
+        }
+        public int getDex()
+        {
+            return dexterity;
+        }
+        public List<Object> getInv()
+        {
+            return inventory;
+        }
+
+        //Set methods
+        public void setName(string n)
+        {
+            name = n;
+        }
+
+        public void setHP(int h)
+        {
+            hp = h;
+        }
+
+        public void setDef(int d)
+        {
+            defense = d;
+        }
+        public void setDex(int d)
+        {
+            dexterity = d;
+        }
+
+
+        //checks if unit has 0 hp
+        public bool isDead()
+        {
+            return (hp == 0);
+        }
+
+        //add item to inventory
+        public void pickUp(Object i)
+        {
+            inventory.Add(i);
+        }
+
+    }
+}
